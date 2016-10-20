c967192e63e69d70569809c438913c31f86afb0b
Christopher Kotfila <chris.kotfila@kitware.com>
2016-10-20 16:47:03 -0400

    Add pytest, coverage, convert first test collection to pytest

--- tests/test_layers_collection.py
+++ tests/test_layers_collection.py
@@ -1,68 +1,84 @@
-import unittest
-
+import pytest
 from geonotebook import layers
 
+@pytest.fixture
+def glc():
+    """ glc: Geonotebook Layer Collection """
 
-class TestGeonotebookLayerCollection(unittest.TestCase):
+    foo = layers.GeonotebookLayer('foo', None, vis_url='vis')
+    bar = layers.GeonotebookLayer('bar', None, vis_url='vis')
+    baz = layers.GeonotebookLayer('baz', None, vis_url='vis')
 
-    def setUp(self):
+    return layers.GeonotebookLayerCollection([foo, bar, baz])
 
-        self.layer_collection = self.generate_layer_collection()
 
-    def test_geonotebooklayercollection_instantiated_with_geonotebooklayers(self):
-        """ Instantiate GeonotebookLayerCollection with list of
-        non GeonotebookLayers should throw Exception """
+def test_geonotebooklayercollection_instantiated_with_geonotebooklayers():
+    """ Instantiate GeonotebookLayerCollection with list of
+    non GeonotebookLayers should throw Exception """
 
-        wrong_layers = [1, 2, 3]
+    with pytest.raises(Exception):
+        layers.GeonotebookLayerCollection([1, 2, 3])
 
-        self.assertRaises(Exception, layers.GeonotebookLayerCollection,
-                          layers=wrong_layers)
 
-    @staticmethod
-    def generate_layer_collection():
-        """ Generates 3 geonotebook layer instances """
+def test_geonotebooklayercollection_has_geonotebooklayer_items(glc):
+    """ GeonotebookLayerCollection.find('foo') should return GeonotebookLayer """
 
-        foo = layers.GeonotebookLayer('foo', None, vis_url='vis')
-        bar = layers.GeonotebookLayer('bar', None, vis_url='vis')
-        baz = layers.GeonotebookLayer('baz', None, vis_url='vis')
+    assert isinstance(glc.find('foo'), layers.GeonotebookLayer)
 
-        return layers.GeonotebookLayerCollection([foo, bar, baz])
+def test_nonexistent_layer_returns_None(glc):
+    """ GeonotebookLayerCollection.find('derp') should return None """
+    assert glc.find('derp') is None
 
-    def test_geonotebooklayercollection_has_geonotebooklayer_items(self):
-        """ GeonotebookLayerCollection.find('foo') should return GeonotebookLayer """
 
-        self.assertTrue(isinstance(self.layer_collection.find('foo'),
-                                   layers.GeonotebookLayer))
+def test_lambda_expressions_with_find(glc):
+    """ GeonotebookLayerCollection.find(lambda g: g.name == 'foo')
+    should return GeonotebookLayer """
 
-    def test_nonexistent_layer_returns_None(self):
-        """ GeonotebookLayerCollection.find('derp') should return None """
+    assert isinstance(glc.find(lambda g: g.name == 'foo'),
+                      layers.GeonotebookLayer)
 
-        self.assertEquals(self.layer_collection.find('derp'),
-                          None)
+def test_lamba_find_with_non_existent_layer(glc):
+    """ GeonotebookLayerCollection.find(lambda g: g.name == 'derp')
+    should return None """
 
-    def test_lambda_expressions_with_find(self):
-        """ GeonotebookLayerCollection.find(lambda g: g.name == 'foo')
-        should return GeonotebookLayer """
+    assert glc.find(lambda g: g.name == 'derp') is None
 
-        self.assertTrue(isinstance(self.layer_collection.find(lambda g: g.name == 'foo'),
-                                   layers.GeonotebookLayer))
+def test_list_indexing_on_layercollection(glc):
+    """ GeonotebookLayerCollection[0] should return GeonotebookLayer
+    and name should equal 'foo'
+    """
+    assert isinstance(glc[0], layers.GeonotebookLayer)
+    assert glc[0].name == 'foo'
 
-    def test_lamba_find_with_non_existent_layer(self):
-        """ GeonotebookLayerCollection.find(lambda g: g.name == 'derp')
-        should return None """
+    assert isinstance(glc[1], layers.GeonotebookLayer)
+    assert glc[1].name == 'bar'
 
-        self.assertEquals(self.layer_collection.find(lambda g: g.name == 'derp'),
-                          None)
+    assert isinstance(glc[2], layers.GeonotebookLayer)
+    assert glc[2].name == 'baz'
 
-    def test_list_indexing_on_layercollection(self):
-        """ GeonotebookLayerCollection[0] should return GeonotebookLayer
-        and name should equal 'foo'
-        """
-        self.assertEquals(self.layer_collection[0].name, 'foo')
+def test_list_indexing_index_error(glc):
+    """ GeonotebookLayerCollection[0] should return GeonotebookLayer
+    and name should equal 'foo'
+    """
+    with pytest.raises(IndexError):
+        glc[4]
 
-    def test_dictionary_key_access_on_layercollection(self):
-        """ GeonotebookLayerCollection['foo'] should return
-        GeonotebookLayer and name should equal 'foo' """
+def test_dictionary_key_access_on_layercollection(glc):
+    """ GeonotebookLayerCollection['foo'] should return
+    GeonotebookLayer and name should equal 'foo' """
 
-        self.assertTrue(isinstance(self.layer_collection['foo'], layers.GeonotebookLayer))
-        self.assertEquals(self.layer_collection['foo'].name, 'foo')
+    assert isinstance(glc['foo'], layers.GeonotebookLayer)
+    assert glc['foo'].name == 'foo'
+
+    assert isinstance(glc['bar'], layers.GeonotebookLayer)
+    assert glc['bar'].name == 'bar'
+
+    assert isinstance(glc['baz'], layers.GeonotebookLayer)
+    assert glc['baz'].name == 'baz'
+
+def test_dictionary_key_access_key_error(glc):
+    """ GeonotebookLayerCollection[0] should return GeonotebookLayer
+    and name should equal 'foo'
+    """
+    with pytest.raises(KeyError):
+        glc['derp']
