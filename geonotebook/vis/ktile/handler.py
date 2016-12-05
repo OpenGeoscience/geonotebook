3cc905ce437f30bcac7ab12ff05d045d035454c3
Christopher Kotfila <chris.kotfila@kitware.com>
2016-12-05 16:28:59 -0500

    Add hardcoded mapnik python API provider for Ktile

--- geonotebook/vis/ktile/handler.py
+++ geonotebook/vis/ktile/handler.py
@@ -1,4 +1,5 @@
 from tornado import web
+
 from notebook.base.handlers import IPythonHandler
 from datetime import datetime, timedelta
 import TileStache as ts
@@ -8,6 +9,29 @@ from .config import KtileConfig
 
 
 def get_config():
+    import gdal, osr
+
+    map_srs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 " + \
+        "+lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m " +\
+        "+nadgrids=@null +wktext +no_defs +over"
+
+    filepath = "/home/kotfic/src/geonotebook/notebooks/data/L57.Globe.month01.2009.hh09vv04.h6v1.doy005to031.NBAR.v3.0.tiff"
+
+    raster = gdal.Open(filepath)
+    srs = osr.SpatialReference()
+    srs.ImportFromWkt(raster.GetProjectionRef())
+    layer_srs = srs.ExportToProj4()
+
+    template = Template(open("/tmp/tmp.xml", "rb").read())
+
+    mapnik_config = bytes(template.render(
+        map_srs=map_srs, layer_srs=layer_srs,
+        filepath=filepath
+    ))
+
+
+
+    #from pudb.remote import set_trace; set_trace(term_size=(319,87))
     config = {
         "cache":
         {
@@ -32,6 +56,19 @@ def get_config():
                 {"name": "mapnik",
                  "mapconfig": "/home/kotfic/src/KTile/examples/style.xml"},
                 "projection": "spherical mercator"
+            },
+
+            "raster2":
+            {
+                "provider": {"class": "geonotebook.vis.ktile.provider:MapnikPythonProvider",
+                             "kwargs": {"path": filepath}}
+            },
+
+            "raster":
+            {
+                "provider":
+                {"name": "mapnik",
+                 "mapfile": mapnik_config}
             }
         }
     }
@@ -74,7 +111,7 @@ class KtileTileHandler(IPythonHandler):
         self.ktile_config_manager = ktile_config_manager
 
     def get(self, kernel_name, layer_name, x, y, z, extension, **kwargs):
-        # from pudb.remote import set_trace; set_trace(term_size=(319,87))
+        #from pudb.remote import set_trace; set_trace(term_size=(319,87))
         config = ts.parseConfig(get_config())
 
         layer = config.layers[layer_name]
---
+++ geonotebook/vis/ktile/provider.py
@@ -0,0 +1,136 @@
+import os
+import gdal, osr
+from thread import allocate_lock
+import mapnik
+
+try:
+    from PIL import Image
+except ImportError:
+    # On some systems, PIL.Image is known as Image.
+    import Image
+
+if 'mapnik' in locals():
+    _version = hasattr(mapnik, 'mapnik_version') and mapnik.mapnik_version() or 701
+
+    if _version >= 20000:
+        Box2d = mapnik.Box2d
+    else:
+        Box2d = mapnik.Envelope
+
+global_mapnik_lock = allocate_lock()
+
+
+DEFAULT_MAP_SRS = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 " + \
+    "+lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m " +\
+    "+nadgrids=@null +wktext +no_defs +over"
+
+
+class MapnikPythonProvider(object):
+    def __init__(self, layer, **kwargs):
+        self.layer = layer
+        self.filepath = kwargs.pop("path", None)
+        self.map_srs = kwargs.pop("map_srs", DEFAULT_MAP_SRS)
+        self.scale_factor = None
+        self._layer_srs = None
+        self.Map = None
+
+    @property
+    def layer_srs(self):
+        if self._layer_srs is None:
+            raster = gdal.Open(self.filepath)
+            srs = osr.SpatialReference()
+            srs.ImportFromWkt(raster.GetProjectionRef())
+            self._layer_srs = srs.ExportToProj4()
+
+        return self._layer_srs
+
+
+    def style_map(self):
+        #<Map font-directory="./fonts" srs="{{map_srs}}">
+        #  <Layer name="raster-layer" srs="{{layer_srs}}" status="on">
+        #    <StyleName>raster-style</StyleName>
+        #    <Datasource>
+        #      <Parameter name="type">gdal</Parameter>
+        #      <Parameter name="file">{{ filepath }}</Parameter>
+        #      <Parameter name="format">tiff</Parameter>
+        #      <Parameter name="band">4</Parameter>
+        #    </Datasource>
+        #  </Layer>
+        #  <Style name="raster-style">
+        #    <Rule>
+        #      <RasterSymbolizer
+        #         opacity="0.8">
+        #      <RasterColorizer default-mode="linear" default-color="white" epsilon="0.001">
+        #        <stop color="blue"        value = "-1"   />
+        #        <stop color="beige"       value = "0"    />
+        #        <stop color="green"       value = "1"    />
+        #      </RasterColorizer>
+        #       <!-- optinal <RasterColorizer/> goes here -->
+        #      </RasterSymbolizer>
+        #    </Rule>
+        #  </Style>
+        #</Map>
+        style = mapnik.Style()
+        rule = mapnik.Rule()
+        sym = mapnik.RasterSymbolizer()
+        sym.opacity = 0.8
+
+        colorizer = mapnik.RasterColorizer(
+            mapnik.COLORIZER_LINEAR,
+            mapnik.Color("white")
+        )
+        colorizer.epsilon = 0.001
+
+        colorizer.add_stop(-1, mapnik.Color("blue"))
+        colorizer.add_stop(0, mapnik.Color("beige"))
+        colorizer.add_stop(1, mapnik.Color("green"))
+
+        sym.colorizer = colorizer
+        rule.symbols.append(sym)
+        style.rules.append(rule)
+
+        self.Map.append_style('Raster Style', style)
+
+        lyr = mapnik.Layer('GDAL Layer from TIFF', self.layer_srs)
+
+        lyr.datasource = mapnik.Gdal(base=os.path.dirname(self.filepath),
+                                     file=os.path.basename(self.filepath),\
+                                     format="tiff",
+                                     band=4)
+        lyr.styles.append("Raster Style")
+
+        self.Map.layers.append(lyr)
+
+    def renderArea(self, width, height, srs, xmin, ymin, xmax, ymax, zoom):
+        """
+        """
+        #
+        # Mapnik can behave strangely when run in threads, so place a lock on the instance.
+        #
+
+        if global_mapnik_lock.acquire():
+            try:
+                if self.Map is None:
+                    self.Map = mapnik.Map(width, height, self.map_srs)
+
+                self.Map.zoom_to_box(Box2d(xmin, ymin, xmax, ymax))
+
+                self.style_map()
+
+                img = mapnik.Image(width, height)
+                # Don't even call render with scale factor if it's not
+                # defined. Plays safe with older versions.
+                if self.scale_factor is None:
+                    mapnik.render(self.Map, img)
+                else:
+                    mapnik.render(self.Map, img, self.scale_factor)
+            except:
+                self.Map = None
+                raise
+            finally:
+                # always release the lock
+                global_mapnik_lock.release()
+
+        img = Image.frombytes('RGBA', (width, height), img.tostring())
+
+        return img
