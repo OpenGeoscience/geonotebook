4f35a205fd853859db3484ed372015398dd84d15
Christopher Kotfila <chris.kotfila@kitware.com>
2016-12-28 06:16:43 -0500

    Add a few integration tests for add_layer and TimeSeries
    These need to be fleshed out more (and currenty TimeSeries
    forward/backward methods are broken)

--- tests/integration/data/get_test_data.sh
+++ tests/integration/data/get_test_data.sh
@@ -2,6 +2,11 @@
 
 if hash aws 2>/dev/null; then
     aws s3 cp s3://golden-tile-geotiffs/L57.Globe.month02.2009.hh09vv04.h6v1.doy032to055.NBAR.v3.0.tiff WELD.tif;
+
+    aws s3 cp s3://golden-tile-geotiffs/L57.Globe.month01.2009.hh09vv04.h6v1.doy005to031.NBAR.v3.0.tiff TimeSeries_01.tif
+    aws s3 cp s3://golden-tile-geotiffs/L57.Globe.month02.2009.hh09vv04.h6v1.doy032to055.NBAR.v3.0.tiff TimeSeries_02.tif
+    aws s3 cp s3://golden-tile-geotiffs/L57.Globe.month03.2009.hh09vv04.h6v1.doy060to087.NBAR.v3.0.tiff TimeSeries_03.tif
+
 else
     echo "AWS CLI script 'aws' is required"
 fi
