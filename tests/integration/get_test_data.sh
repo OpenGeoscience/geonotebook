bf3caadf5b7d9ed9f267e29d0977a5928e6387ae
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-02-08 13:42:56 -0500

    Add new files to download script

--- tests/integration/get_test_data.sh
+++ tests/integration/get_test_data.sh
@@ -1,9 +1,27 @@
 #/bin/bash
+set -e
 
-GIRDER_DATA_URL=https://data.kitware.com/api/v1/folder/586a75798d777f1e3428d829/download
+dir="$(dirname $0)"
 
-TMPFILE=`mktemp`
+function gettemp() {
+    TMPFILE=`mktemp`
+}
 
-wget $GIRDER_DATA_URL -O $TMPFILE
-unzip -d `pwd` $TMPFILE
+function download() {
+    gettemp
+    curl $1 -o $TMPFILE
+}
+
+# tifs
+download 'https://data.kitware.com/api/v1/folder/586a75798d777f1e3428d829/download'
+unzip -o -d "$dir" $TMPFILE
 rm $TMPFILE
+
+# shapefile
+download 'https://data.kitware.com/api/v1/item/589b390c8d777f07219fcb3e/download'
+unzip -o -d "$dir/data/states_20m" $TMPFILE
+rm $TMPFILE
+
+# geojson
+download 'https://data.kitware.com/api/v1/item/589b624d8d777f07219fcc60/download'
+mv $TMPFILE "$dir/data/ne_50m_populated_places_simple.geojson"
